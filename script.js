9378e36325383589f70b1b0c2fd771828419e503
QiaoLin22 <qiao.lin@emory.edu>
2020-04-11 20:01:50 -0400

    cc

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+  if (5 > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
     startButton.innerText = 'Restart'
